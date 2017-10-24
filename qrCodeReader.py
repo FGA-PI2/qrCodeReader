a8c50e4d3bdea194362113ecf8871aefb784f502
gustavo sabino <gustavo.sabino@hotmail.com>
2017-10-24 20:55:03 -0200

    Subtract the volume from the request drink from remaining_volume in bebida

--- qrCodeReader.py
+++ qrCodeReader.py
@@ -3,6 +3,9 @@ from sys import argv
 import zbar
 import json
 import requests
+
+
+API_URL = 'http://dev-pi2-api.herokuapp.com/'
 quantity = 0
 # create a Processor
 proc = zbar.Processor()
@@ -18,9 +21,9 @@ proc.init(device)
 
 # setup a callback
 
-def update(url,qr_code):
+def update(url,data):
     headers = {'X-HTTP-Method-Override':'PATCH'}
-    request = requests.put(url,data=qr_code, headers=headers)
+    request = requests.put(url,data=data, headers=headers)
 
     return request
 
@@ -35,43 +38,73 @@ def processQRCode(qrcode):
 
     data = qrcode["data_compra"]
     user = qrcode["usuario"]
-    responseFromAPI = requests.get("http://dev-pi2-api.herokuapp.com/compra/?data_compra={}&usuario__id={}".format(data, user))
+    responseFromAPI = requests.get(API_URL+"compra/?data_compra={}&usuario__id={}".format(data, user))
 
     result  = json.loads(responseFromAPI.content)
-    # print result[0]
-    print("Antes do if")
+
     if (result[0]["qr_code"]["is_valid"] == True):
-        print ("QR Code Valido!")
-        print ("O pedido %s sera feito!" %result[0]["nome"])
-        print (result[0]["qr_code"]["is_valid"])
+
+
+        bebidas_list = list()
+        for item,index in zip(result[0]['pedido'],len(result[0]['pedido'])):
+
+            bebida[index] = (
+                requests.get(API_URL+"bebida/{}".format(item['bebida'])).content,
+                item
+            )
+
+            if bebida[index]['remaining_quantity'] < item['volume']:
+                print("Não há {} o suficiente para fazer esta bebida. Entre em contato com o Administrador").format(bebida[index]['nome'])
+                enough_bebida = False
+
+            else:
+                enough_bebida = True
+
+
         #SEND ALL DATAS TO ELETRONIC HERE
-        result[0]["qr_code"]["is_valid"] = False
-        url = "http://dev-pi2-api.herokuapp.com/code/{}/".format(result[0]["qr_code"]["id"])
-        qrcode = {
-    "is_valid": False,
-    "qr_code": result[0]["qr_code"]["qr_code"],
-    "usuario": result[0]["qr_code"]["usuario"]
-} 
-        # print qrcode
-        print(url)
-        print("DEpois do print - - TRUE")
-        r = update(url,qrcode)
-        # print(requests.put(url, data=qrcode))
-        print("PUT: ",r)
+        if enough_bebida is True:
+
+            list_of_orders = list()
+
+            for item in result[0]['pedido']:
+
+                drink_to_make = {
+                    'posicao' : item['bebida']['posicao'],
+                    'volume' : item['volume']
+                }
+
+                #Essa lista é o que eletronica deve ler para realizar o pedido
+                list_of_orders.append(drink_to_make)
+
+
+
+            result[0]["qr_code"]["is_valid"] = False
+            url = API_URL+"code/{}/".format(result[0]["qr_code"]["id"])
+
+            qrcode = {
+                "is_valid": False,
+                "qr_code": result[0]["qr_code"]["qr_code"],
+                "usuario": result[0]["qr_code"]["usuario"]
+            }
+
+            update_qr_code_status = update(url,qrcode)
+
+            for bebida in bebidas_list:
+                subtract_volume_of_drink = update(API_URL+"bebida/{}/".format(bebida[0]['nome']),{"remaining_quantity":bebida[1]['nome']})
 
     else:
         print ("QR Code Invalido!")
         print ("Este pedido ja foi retirado!")
         print (result[0]["qr_code"]["is_valid"])
 
-def my_handler(proc, image, closure):  
-    
+def my_handler(proc, image, closure):
+
     #for symbol in image.symbols:
     for symbol in image:
         # do something useful with results
         print "OI"
-		# convert the symbol.data (aka the data extracted from the qr code)	to the string format	
-        qrCodeAsString = '"%s"' % symbol.data
+		# convert the symbol.data (aka the data extracted from the qr code)	to the string format
+        qrCodeAsString = '"{}"'.format(symbol.data)
         print qrCodeAsString
         # deletes the first the " punctuation so we can load the text string as JSON data
         qrCodeAsString = qrCodeAsString[1:len(qrCodeAsString)-1]
@@ -79,34 +112,7 @@ def my_handler(proc, image, closure):
         qrCodeAsJSON = json.loads(qrCodeAsString)
 
         processQRCode(qrCodeAsJSON)
-       
 
-        # print "Usuario da compra (API): %s" % result[0]["usuario"]
-        # print "Data de compra (API): %s" % result["data_compra"]
-        # print "Is valid? (API): " + str(result["qr_code"]["is_valid"])
-        # print "--------------------"
-
-
-
-        #print "User ID: %s" % qrCodeAsJSON["usuario"]
-        #print "JSON Bebida: %s" % qrCodeAsJSON["pedido"][0]
-        #print "Bebida 1: %s" % qrCodeAsJSON["pedido"][0]["bebida"]
-        #print "Porcentagem Bebida 1: %s " % qrCodeAsJSON["pedido"][0]["porcentagem"]
-        #print "Bebida 2: %s" % qrCodeAsJSON["pedido"][1]["bebida"]
-        #print "Porcentagem Bebida 2: %s " % qrCodeAsJSON["pedido"][1]["porcentagem"]
-        #if qrCodeAsJSON["gelo"]:
-       # 	print "Gelo: Sim"
-        #else:
-        #	print "Gelo: Nao"
-        #print "Tamanho do copo: %s ml" % qrCodeAsJSON["tamanho"]  
-        
-        #print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
-#        if symbol.data == "teste.org":
-#        	print "teste 1"
-#        if symbol.data == "http://teste2.org":
-#        	print "teste 2"
-#        else:
-#        	print "nao ta dando"
 
 proc.set_data_handler(my_handler)
 
